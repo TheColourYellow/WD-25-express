abaf9bb78147c3448b18847e9c7d7ec95041c60b
Patrik <Patrik.Granstrom@metropolia.fi>
2025-04-02 10:37:54 +0300

    Add preliminary implementation of authentication

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -1,1 +1,16 @@
 //SAmat metodit kuin cat, mutta user tilalle
+//import bcrypt from 'bcrypt';
+
+/*const postUser = async (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, 10);
+  req.body.filename = req.file.filename;
+  const result = await addCat(req.body);
+  console.log(`Form data: $${result}`);
+  //console.log(`File data: ${req.file}`);
+  if (result.cat_id) {
+    res.status(201);
+    res.json({message: 'New cat added.', result});
+  } else {
+    res.sendStatus(400);
+  }
+};*/
--- src/api/models/user-models.js
+++ src/api/models/user-models.js
@@ -1,1 +1,6 @@
 //Samat metodit kuin cat mutta user tilalle
+/*const login = async (user) => {
+  const sql = `SELECT *
+              FROM wsk_users
+              WHERE username = ?`;
+};*/
