af2eed0e2109e6e9084a570a78f0a3cca5d66c1a
Patrik <Patrik.Granstrom@metropolia.fi>
2025-04-03 17:59:25 +0300

    Add routes, methods and endpoints for users

--- src/api/models/user-models.js
+++ src/api/models/user-models.js
@@ -1,1 +1,42 @@
-//Samat metodit kuin cat mutta user tilalle
+const userItems = [
+  {
+    user_id: 3609,
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'john@metropolia.fi',
+    role: 'user',
+    password: 'password',
+  },
+  {
+    user_id: 800,
+    name: 'Fict Name',
+    username: 'Fiction',
+    email: 'fict@metropolia.fi',
+    role: 'user',
+    password: 'password1',
+  },
+];
+
+const listAllUsers = () => {
+  return userItems;
+};
+
+const findUserById = (id) => {
+  return userItems.find((item) => item.user_id == id);
+};
+
+const addUser = (user) => {
+  const {name, username, email, role, password} = user;
+  const newId = userItems[0].user_id + 1;
+  userItems.unshift({
+    user_id: newId,
+    name,
+    username,
+    email,
+    role,
+    password,
+  });
+  return {user_id: newId};
+};
+
+export {listAllUsers, findUserById, addUser};
--- src/api/routes/user-router.js
+++ src/api/routes/user-router.js
@@ -1,1 +1,16 @@
-//Samat metodit kuin cat mutta user tilalle
+import express from 'express';
+import {
+  getUser,
+  getUserById,
+  postUser,
+  putUser,
+  deleteUser,
+} from '../controllers/user-controller.js';
+
+const userRouter = express.Router();
+
+userRouter.route('/').get(getUser).post(postUser);
+
+userRouter.route('/:id').get(getUserById).put(putUser).delete(deleteUser);
+
+export default userRouter;
